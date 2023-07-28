c4692ce7257373e19259603e7f8c3268d029723a
zqlanly <48847308+zqlanly@users.noreply.github.com>
2023-07-28 16:13:38 +0800

    _wokrer.js

--- _wokrer.js
+++ _wokrer.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.hicairo.com'
+      url.hostname = 'ba4app-zqlanly.b4a.run'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
